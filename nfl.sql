39689913f070d92659a1dee714faf95f1a6c6c9e
adriancgarcia <adriancgarcia@gmail.com>
2023-09-17 17:21:41 -0500

    NFL db Seeded, queries 1, quesries2, quesries3 complete

--- nfl.sql
+++ nfl.sql
@@ -1,21 +1,3 @@
-create table teams(
-	id serial primary key,
-	name varchar(255) not null,
-	stadium varchar(255),
-	division varchar(255),
-	conference varchar(255),
-	head_coach varchar(255),
-	active boolean
-);
-
-create table players(
-	id serial primary key,
-	name varchar(255) not null,
-	position varchar(255),
-	salary integer,
-	team_id integer references teams
-);
-
 -- 1. List the names of all NFL teams'
 -- SELECT name FROM teams;
 
@@ -32,25 +14,43 @@ create table players(
 -- FROM players;
 
 -- 5. The team names and head coaches of the NFC North and AFC East
-
+-- SELECT name, head_coach FROM teams
+-- WHERE conference = 'NFC'
+-- AND division LIKE 'North'
+-- OR conference = 'AFC'
+-- AND division LIKE 'East';
 
 -- 6. The 50 players with the highest salaries
-
+-- SELECT name 
+-- FROM players
+-- ORDER BY salary DESC
+-- LIMIT 50;
 
 -- 7. The average salary of all NFL players
-
+-- SELECT AVG(Salary)
+-- FROM players;
 
 -- 8. The names and positions of players with a salary above 10_000_000
-
+-- SELECT name, position
+-- FROM players
+-- WHERE salary > 10000000;
 
 -- 9. The player with the highest salary in the NFL
-
+-- SELECT name
+-- FROM players
+-- ORDER BY salary DESC
+-- LIMIT 1;
 
 -- 10. The name and position of the first 100 players with the lowest salaries
-
+-- SELECT name, position
+-- FROM players
+-- ORDER BY salary ASC
+-- limit 100;
 
 -- 11. The average salary for a DE in the nfl
-
+-- SELECT AVG (Salary)
+-- FROM players
+-- WHERE position LIKE 'DE';
 
 -- 12. The names of all the players on the Buffalo Bills
 
